4a401b0101749c99faff65e642ae3e1c6ea96063
Leandro Alexandrino <hello@meche.space>
2016-07-06 21:45:44 -0300

    create webpack config

---
+++ dev/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <p>my name is Leandro... or Leandrinoo</p>
+        <ul>
+          <li> Item </li>
+          <li> Item </li>
+          <li> Item </li>
+          <li> Item </li>
+          <li> Item </li>
+        </ul>
+        <h2>The tests and more tests</h2>
+        <p>Try make a application with React</p>
+        <h2>Fine!</h2>
+        <p>Thank you!</p>
+      </div>
+    )
+  }
+}
+
+export default App
