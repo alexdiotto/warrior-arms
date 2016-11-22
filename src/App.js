f9dfa2f1cd024261a3c30b40c5dc7727e0f31f14
Leandro Alexandrino <hello@meche.space>
2016-11-22 20:24:31 -0200

    change initial config

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 'use strict'
 
 import React from 'react'
+import Title from 'components/title'
 
 class App extends React.Component {
   render () {
     return (
       <div>
-        <h1>Hello World!</h1>
-        <p>my name is Leandro... or Leandrinoo</p>
-        <ul>
-          <li> Item </li>
-          <li> Item </li>
-        </ul>
-        <h2>The tests and more tests</h2>
-        <p>Try make a application with React</p>
-        <h2>Fine!</h2>
-        <p>Thank you!</p>
-        <ElementFolder />
+        <Title>My App</Title>
       </div>
     )
   }
 }
-
-var ElementFolder = React.createClass({
-  render: function () {
-    return (
-      <small>MIT Licence 2016</small>
-    )
-  }
-})
 
 export default App
