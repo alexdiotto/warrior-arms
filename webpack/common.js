fec5b70db5f7e2cbf731d2456fe341566f571035
Leandro Alexandrino <hello@meche.space>
2017-01-26 21:20:20 -0200

    replace CSS Modules name class CSS

--- webpack/common.js
+++ webpack/common.js
@@ -35,7 +35,7 @@ module.exports = {
     include: /src/,
     loaders: [
       'style-loader',
-      'css-loader?sourceMap&modules!postcss-loader?sourceMap'
+      'css-loader?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader?sourceMap'
     ]
   },
 
