69c1952e110d832eb741cb4470407fe320fa6578
Leandro Alexandrino <hello@meche.space>
2017-02-19 19:43:50 -0300

    Change extrac-text-webpack-plugin to beta version

--- warrior.sh
+++ warrior.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-DEV_LIST="autoprefixer babel-core babel-jest babel-loader babel-preset-es2015 babel-preset-react babel-preset-stage-0 chai cross-env css-loader extract-text-webpack-plugin html-webpack-plugin jest-cli postcss-cssnext postcss-import postcss-loader react-hot-loader@3.0.0-beta.6 standard standard-loader style-loader webpack webpack-dashboard webpack-dev-server webpack-validator"
+DEV_LIST="autoprefixer babel-core babel-jest babel-loader babel-preset-es2015
+babel-preset-react babel-preset-stage-0 chai cross-env css-loader
+extract-text-webpack-plugin@beta html-webpack-plugin jest-cli postcss-cssnext postcss-import postcss-loader react-hot-loader@3.0.0-beta.6 standard standard-loader style-loader webpack webpack-dashboard webpack-dev-server webpack-validator"
 
 PROD_LIST="react react-dom"
 
