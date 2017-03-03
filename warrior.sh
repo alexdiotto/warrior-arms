8de7035897ae5a6febdf02866af63086a279bbc6
Leandro Alexandrino <leandro@leandrino.me>
2017-03-02 21:23:49 -0300

    remove package

--- warrior.sh
+++ warrior.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-DEV_LIST="autoprefixer babel-core babel-jest babel-loader babel-preset-es2015
-babel-preset-react babel-preset-stage-0 chai cross-env css-loader
-extract-text-webpack-plugin@beta html-webpack-plugin jest-cli postcss-cssnext postcss-import postcss-loader react-hot-loader@3.0.0-beta.6 standard standard-loader style-loader webpack webpack-dashboard webpack-dev-server webpack-validator"
+DEV_LIST="autoprefixer babel-core babel-jest babel-loader babel-preset-es2015 babel-preset-react babel-preset-stage-0 chai cross-env css-loader extract-text-webpack-plugin@beta html-webpack-plugin jest-cli postcss-cssnext postcss-import postcss-loader react-hot-loader@3.0.0-beta.6 standard standard-loader style-loader webpack webpack-dashboard webpack-dev-server "
 
 PROD_LIST="react react-dom"
 
