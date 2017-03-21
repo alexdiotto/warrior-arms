8c568fa4dd994cb9d9fcb311343019f0bb1c4d2e
Leandro Alexandrino <leandro@leandrino.me>
2017-03-20 23:08:14 -0300

    fix bug storybook withou css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react'
 
-import './css/style.css'
+import './App.css'
 
 class App extends Component {
   constructor () {
--- webpack/storybook.config.js
+++ webpack/storybook.config.js
@@ -8,17 +8,28 @@ module.exports = (config, env) => {
 
   const preloaders = Object.assign({}, common.standardPreLoader, {
     use: undefined,
-    loader: common.standardPreLoader.use
+    loader: common.standardPreLoader.use.loader + '?' +
+    common.standardPreLoader.use.options.parser
   })
 
-  const cssloaders = Object.assing({}, common.cssLoader, {
-    use: undefined,
-    loader: common.cssLoader.use
-  })
+  // const cssloaders = Object.assign({}, common.cssLoader, {
+  //   use: undefined,
+  //   loaders: [
+  //     common.cssLoader.use[0].loader + '?' +
+  //     common.cssLoader.use[0].options.sourceMap,
+  //     common.cssLoader.use[1].loader + '?' +
+  //     common.cssLoader.use[1].options.sourceMap + '&' +
+  //     common.cssLoader.use[1].options.importLoaders + '&' +
+  //     common.cssLoader.use[1].options.modules + '&' +
+  //     common.cssLoader.use[1].options.localIdentName,
+  //     common.cssLoader.use[2].loader
+  //   ]
+  // })
 
   newConfig.module.preLoaders = (newConfig.module.preLoaders || []).concat(preloaders)
-  newConfig.module.loaders = (newConfig.moduleloaders || []).concat(cssloaders)
+  // newConfig.module.loaders = (newConfig.moduleloaders || []).concat(cssloaders)
   newConfig.resolve = common.resolve
+  newConfig.postcss = common.cssLoader.use[2].options
 
   return newConfig
 }
